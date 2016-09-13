4b013bde5580b0efa868ceea69af96e0c73dc634
SheGe <szymon@devid.pl>
2016-09-13 11:45:40 +0200

    Change BindingProtocol enum to class

--- src/IIS/Types/BindingProtocol.cs
+++ src/IIS/Types/BindingProtocol.cs
@@ -1,9 +1,51 @@
-﻿namespace Cake.IIS
+﻿using System;
+
+namespace Cake.IIS
 {
-    public enum BindingProtocol
+    public sealed class BindingProtocol
     {
-        Ftp,
-        Http,
-        Https
+        public static BindingProtocol Ftp
+        {
+            get { return new BindingProtocol(Uri.UriSchemeFtp); }
+        }
+
+        public static BindingProtocol Http
+        {
+            get { return new BindingProtocol(Uri.UriSchemeHttp); }
+        }
+
+        public static BindingProtocol Https
+        {
+            get { return new BindingProtocol(Uri.UriSchemeHttps); }
+        }
+
+        private BindingProtocol(string name)
+        {
+            Name = name;
+        }
+
+        private string Name { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        private bool Equals(BindingProtocol other)
+        {
+            return string.Equals(Name, other.Name, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            return obj is BindingProtocol && Equals((BindingProtocol)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return (Name != null ? Name.GetHashCode() : 0);
+        }
     }
 }
