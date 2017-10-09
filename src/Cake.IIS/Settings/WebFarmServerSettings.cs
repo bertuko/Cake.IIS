02f2424c75e79ece3993d119565e57496f99819e
filipe.esch <filipe.esch@cnova.com>
2017-10-09 18:31:28 -0300

    -Created WebFarmServerSettings to specify WebFarm servers attributes -Fix bug in AddServer

---
+++ src/Cake.IIS/Settings/WebFarmServerSettings.cs
@@ -0,0 +1,25 @@
+﻿namespace Cake.IIS
+{
+    public class WebFarmServerSettings
+    {
+        #region Constructor
+        public WebFarmServerSettings()
+        {
+        }
+        #endregion
+
+
+
+
+
+        #region Properties
+        public string Address { get; set; }
+
+        public int? HttpPort { get; set; }
+
+        public int? HttpsPort { get; set; }
+
+        public int? Weight { get; set; }
+        #endregion
+    }
+}
